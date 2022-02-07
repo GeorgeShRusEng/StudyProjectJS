1596fddcc70fd61f7e5d6af365d9bbd6e74f8fc7
Egor <example@mail.com>
2022-02-08 00:38:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+/*let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+const obj={
+    a:50
+};
+obj.a=70;
+console.log(obj);*/
+let number = 4.6;
+console.log(4 / 0);
+console.log('string' * 9);
+const person = `Alex`;
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 15,
+    iaMarried: false
+};
+console.log(obj.name);
+/*//!console.log(obj["name"]); так писать нежелательно*/
+
+/*alert('Wellcome');*/
+
+/*const result=confirm("A you here?");
+console.log(result);*/
+
+//const answer = +prompt("Вам есть 18 лет?","18");
+// console.log(answer+5);
+
+/*const answers = [];
+answers[0]=prompt('What is you name?','');
+answers[1]=prompt('What is you cat name?','');
+answers[2]=prompt('How wold a you??','');
+//document.write(answers);/*/
+/!Устаревшая команда, заменяет собой все соддержимое страницы */
+//console.log(typeof(answers));
+
+const category = 'Toys';
+console.log(`http://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет ${user}`);
+
+let incr = 10,
+    decr = 10;
+    /*++incr;
+    --decr;*/
+    //console.log(incr++,decr--);//!Постфиксная форма
+    console.log(++incr,--decr);//!Префиксная форма 
+
